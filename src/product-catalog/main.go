b3c43bd7d528189f5dcce9a21d3bda55333f5010
parameshnaathi <naathiparamesh143@gmail.com>
2026-09-17 10:37:26 +0530

    testing code-quality check

--- src/product-catalog/main.go
+++ src/product-catalog/main.go
@@ -252,7 +252,7 @@ func (p *productCatalog) GetProduct(ctx context.Context, req *pb.GetProductReque
 
 	// GetProduct will fail on a specific product when feature flag is enabled
 	if p.checkProductFailure(ctx, req.Id) {
-		msg := fmt.Sprintf("Error: Product Catalog Fail Feature Flag Enabled")
+		msg := "Error: Product Catalog Fail Feature Flag Enabled"
 		span.SetStatus(otelcodes.Error, msg)
 		span.AddEvent(msg)
 		return nil, status.Errorf(codes.Internal, msg)
@@ -304,19 +304,14 @@ func (p *productCatalog) checkProductFailure(ctx context.Context, id string) boo
 
 	client := openfeature.NewClient("productCatalog")
 	failureEnabled, _ := client.BooleanValue(
-		ctx, "productCatalogFailure", false, openfeature.EvaluationContext{}, 
+		ctx, "productCatalogFailure", false, openfeature.EvaluationContext{},
 	)
 	return failureEnabled
 }
 
-func createClient(ctx context.Context, svcAddr string) (*grpc.ClientConn, error) {
-	return grpc.DialContext(ctx, svcAddr,
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-		grpc.WithStatsHandler(otelgrpc.NewClientHandler()),
-	)
-}
-
-
-
-
-
+// func createClient(ctx context.Context, svcAddr string) (*grpc.ClientConn, error) {
+// 	return grpc.DialContext(ctx, svcAddr,
+// 		grpc.WithTransportCredentials(insecure.NewCredentials()),
+// 		grpc.WithStatsHandler(otelgrpc.NewClientHandler()),
+// 	)
+// }
